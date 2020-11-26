4ddcfdabd54db0429aa3d7a5b7e4f0e86985b6e7
Abousateur1 <aboura5red@gmail.com>
2020-11-26 17:16:50 -0500

    Added a join, leave and play music command to the bot

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from pafy import new
+from discord import FFmpegPCMAudio
 
 load_dotenv()
 
@@ -18,7 +20,36 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
+    
+@client.command()
+async def hello(ctx):
+    await ctx.send(f'Hello World!')
 
+@client.command(pass_context = True)
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    await ctx.voice_client.disconnect()        
+
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    ffmpeg_opts = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    channel = ctx.author.voice.channel
+    if not client.voice_clients:
+        voice = await channel.connect()
+    else:
+        voice = client.voice_clients[0]
+    await ctx.send("**Playing** Music")
+    video = new(url)
+    audio = video.getbestaudio().url
+    
+    voice.play(FFmpegPCMAudio(audio, **ffmpeg_opts))
+    voice.source = discord.PCMVolumeTransformer(voice.source, volume=0.25)
+    voice.is_playing()
 
         
 client.run(SECRET_KEY)
